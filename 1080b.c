39cc05aa5eb27d06825e941b54e73811b757e922
Luís Felippe <318655966+Luis-FppGP@users.noreply.github.com>
2026-09-19 14:16:58 -0300

    Create 1080b.c

---
+++ 1080b.c
@@ -0,0 +1,37 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Algortimo e Estrutura de Dados 2026S1
+Nome        : Luis Felippe Guimaraes Pinheiro
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1080
+Data        : 15/09/2026
+Objetivo    : Mostrar o maior valor de um vetor de 100 inteiros positivos.
+Dificuldade : Nenhum, o problema e simples, mas por lguma razão não estava tendo acertos corretor no judge
+Uso de IA   : Usei IA porque o judge estava colocando meu problema como errado mesmo apos eu corrigi-lo varias vezes, mas ainda assim 
+              nao foi o suficiente
+-------------------------------------------------------------------------- */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main() {
+ 
+    int *N, indice, maior;
+    
+    N = (int *) malloc (100 * sizeof(int));
+    
+    for(int i = 0; i < 100; i++){
+        scanf("%d", &N[i]);
+        if(i == 0) maior = N[0];
+        if(maior < N[i]){
+            maior = N[i];
+            indice = i;
+        }
+    }
+    
+    printf("%d\n%d\n", maior, indice + 1);
+    
+    free(N)
+ 
+    return 0;
+}
